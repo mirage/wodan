a52f79c5ebe22e5827cddb8423551847ce99d0f4
Gabriel <g2p.code@gmail.com>
2017-03-04 11:52:58 +0000

    Make ramdisks and file devices switchable in mirage configure
    Requires https://github.com/mirage/mirage/pull/800

--- config.ml
+++ config.ml
@@ -1,13 +1,13 @@
 open Mirage
 
 let client =
-  let packages = [ package "mirage-logs"; package "nocrypto"; package "io-page"; package "lru-cache"; package "bitv"; package "mirage-block-ramdisk"; ] in
+  let packages = [ package "mirage-logs"; package "nocrypto"; package "io-page"; package "lru-cache"; package "bitv"; ] in
   foreign
     ~packages
     ~deps:[abstract nocrypto]
     "Unikernel.Client" @@ console @-> block @-> job
 
 let () =
-  let img = block_of_file "disk.img" in
-  let job =  [ client $ default_console $ img ] in
+  let img = generic_block "disk.img" in
+  let job = [ client $ default_console $ img ] in
   register "storage" job
