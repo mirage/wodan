7de14db443685ceca2152ad5f4ba9690a747a4be
Gabriel <g2p.code@gmail.com>
2017-03-06 09:32:45 +0000

    Periodically reopen the filesystem in tests

--- unikernel.ml
+++ unikernel.ml
@@ -1,4 +1,5 @@
 open Mirage_types_lwt
+open Lwt.Infix
 
 module Client (C: CONSOLE) (B: BLOCK) = struct
   module Stor = Storage.Make(B)(Storage.StandardParams)
@@ -8,27 +9,32 @@ module Client (C: CONSOLE) (B: BLOCK) = struct
     let%lwt roots = Stor.prepare_io (Storage.FormatEmptyDevice
       Int64.(div (mul info.size_sectors @@ of_int info.sector_size) @@ of_int Storage.StandardParams.block_size)) disk 1024 in
     (*let%lwt roots1 = Stor.prepare_io Storage.OpenExistingDevice disk 1024 in*)
-    let root = Storage.RootMap.find 1l roots in
+    let root = ref @@ Storage.RootMap.find 1l roots in
     (
     let key = Cstruct.of_string "abcdefghijklmnopqrst" in
     let cval = Cstruct.of_string "sqnlnfdvulnqsvfjlllsvqoiuuoezr" in
-    let () = Stor.insert root key cval in
-    let%lwt () = Stor.flush root.open_fs in
-    let%lwt () = Stor.flush root.open_fs in
-    let%lwt cval1 = Stor.lookup root key in
+    let () = Stor.insert !root key cval in
+    let%lwt () = Stor.flush !root.open_fs in
+    let%lwt cval1 = Stor.lookup !root key in
     (*let () = Cstruct.hexdump cval1 in*)
     let () = assert (Cstruct.equal cval cval1) in
     let%lwt roots = Stor.prepare_io Storage.OpenExistingDevice disk 1024 in
-    let root = Storage.RootMap.find 1l roots in
-    let%lwt cval2 = Stor.lookup root key in
+    root := Storage.RootMap.find 1l roots;
+    let%lwt cval2 = Stor.lookup !root key in
     let () = assert (Cstruct.equal cval cval2) in
     while%lwt true do
       let key = Nocrypto.Rng.generate 20 and
         cval = Nocrypto.Rng.generate 40 in
-      Stor.insert root key cval;
-      if%lwt Lwt.return (Nocrypto.Rng.Int.gen 16 = 0) then (* 1/16 chance of flushing *)
-        Stor.flush root.open_fs
+      Stor.insert !root key cval;
+      if%lwt Lwt.return (Nocrypto.Rng.Int.gen 16384 = 0) then begin (* Infrequent re-opening *)
+        let%lwt () = Stor.flush !root.open_fs in
+        let%lwt roots = Stor.prepare_io Storage.OpenExistingDevice disk 1024 in
+        root := Storage.RootMap.find 1l roots;
+        Lwt.return ()
+      end >>= function () ->
+      if%lwt Lwt.return (Nocrypto.Rng.Int.gen 8192 = 0) then (* Infrequent flushing *)
+        Stor.flush !root.open_fs
     done
     )
-      [%lwt.finally Lwt.return @@ Stor.log_statistics root]
+      [%lwt.finally Lwt.return @@ Stor.log_statistics !root]
 end
