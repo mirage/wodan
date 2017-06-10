b44c198c11ec741190ea46a640b6a431c98533ed
Gabriel <g2p.code@gmail.com>
2017-06-10 20:37:47 +0900

    Fix a precedence bug

--- runner/unikernel.ml
+++ runner/unikernel.ml
@@ -23,14 +23,15 @@ module Client (C: CONSOLE) (B: BLOCK) = struct
     while%lwt true do
       let key = Stor.key_of_cstruct @@ Nocrypto.Rng.generate 20 and
         cval = Stor.value_of_cstruct @@ Nocrypto.Rng.generate 40 in
-      try%lwt
+      begin try%lwt
         Stor.insert root key cval
       with Storage.NeedsFlush -> begin
         Logs.info (fun m -> m "Emergency flushing");
         Stor.flush root >>= function _gen ->
         Stor.insert root key cval
       end
-      >>= function () ->
+      end
+      >>
       if%lwt Lwt.return (Nocrypto.Rng.Int.gen 16384 = 0) then begin (* Infrequent re-opening *)
         Stor.flush root >>= function gen3 ->
         let%lwt root, gen4 = Stor.prepare_io Storage.OpenExistingDevice disk 1024 in
