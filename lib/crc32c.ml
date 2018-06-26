00dc07fadd92ed8586d1c90a06607ca76238770a
Gabriel <g2p.code@gmail.com>
2018-06-26 16:15:21 +0200

    Use safe conversion functions, and fix a typo

--- lib/crc32c.ml
+++ lib/crc32c.ml
@@ -2,8 +2,20 @@
 
 let (~~~) = Int32.lognot
 
+(* XXX x64 only *)
+let optint_sign = Optint.of_int 0x8000_0000
+
+let optint_of_uint32 i =
+  if i < 0l then
+    Optint.(logor optint_sign @@ of_int32 @@ Int32.neg i)
+  else Optint.of_int32 i
+
+let optint_to_uint32 i =
+  Optint.to_int32 i
+
+
 let cstruct ?(crc=0l) cstr =
-  Optint.to_int32 @@ Checkseum.Adler32.digest_bigstring (Cstruct.to_bigarray cstr) 0 (Cstruct.len cstr) (Optint.of_int32 crc)
+  optint_to_uint32 @@ Checkseum.Crc32c.digest_bigstring (Cstruct.to_bigarray cstr) 0 (Cstruct.len cstr) (optint_of_uint32 crc)
 
 let cstruct_valid str =
   ~~~(cstruct str) = 0l
