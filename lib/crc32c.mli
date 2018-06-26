49522aae5f5e17a6faee9b5504e18a3ba110dee0
Gabriel <g2p.code@gmail.com>
2018-06-26 14:39:52 +0200

    Move CRC32C to @dinosaure's checkseum

--- lib/crc32c.mli
+++ lib/crc32c.mli
@@ -1,13 +1,8 @@
-(* Copyright 2014 Citrix - ISC licensed *)
+(* Copyright 2014 Citrix - ISC licensed *)
 
 val cstruct: ?crc:int32 -> Cstruct.t -> int32
 (** [cstruct ?crc buf] computes the CRC32C of [buf] with optional
     initial value [crc] *)
-
-val string: ?crc:int32 -> string -> int -> int -> int32
-(** [string ?crc buf ofs len] computes the CRC32C of the substring
-    of length [len] starting at offset [ofs] in string [buf] with
-    optional initial value [crc] *)
 
 val cstruct_valid: Cstruct.t -> bool
 (** [cstruct_valid cstruct] returns whether the CRC32C of cstruct is
