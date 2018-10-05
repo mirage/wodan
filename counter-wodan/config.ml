e1a078862d5cba1c0415610adc7f30d9c6ff27dd
Martin Lucina <martin@lucina.net>
2018-10-05 17:48:20 +0200

    Add camel service work in progress

---
+++ counter-wodan/config.ml
@@ -0,0 +1,25 @@
+open Mirage
+
+let stack = generic_stackv4 default_network
+(* set ~tls to false to get a plain-http server *)
+let http_srv = http_server @@ conduit_direct ~tls:false stack
+
+let http_port =
+  let doc = Key.Arg.info ~doc:"Listening HTTP port." ["http"] in
+  Key.(create "http_port" Arg.(opt int 8080 doc))
+
+let main =
+  let packages = [
+      package "uri";
+      package "wodan";
+      package ~sublibs:["ocaml"] "checkseum";
+  ] in
+  let keys = List.map Key.abstract [ http_port ] in
+  foreign
+    ~packages ~keys
+    "Dispatch.HTTP" (pclock @-> http @-> block @-> job)
+
+let img = block_of_file "disk.img"
+
+let () =
+  register "http" [main $ default_posix_clock $ http_srv $ img]
---
+++ counter-wodan/dispatch.ml
@@ -0,0 +1,98 @@
+open Lwt.Infix
+open Mirage_types_lwt
+
+(** Common signature for http and https. *)
+module type HTTP = Cohttp_lwt.S.Server
+
+(* Logging *)
+let http_src = Logs.Src.create "http" ~doc:"HTTP server"
+module Http_log = (val Logs.src_log http_src : Logs.LOG)
+
+module Dispatch (Store: Wodan.S) (S: HTTP) = struct
+
+  let failf fmt = Fmt.kstrf Lwt.fail_with fmt
+
+  let key = Store.key_of_string "12345678901234567890"
+
+  let next_camel store =
+        Store.lookup store key >>= function 
+            Some counter ->
+              let c = Int64.of_string @@ Store.string_of_value counter in
+              Store.insert store key @@ Store.value_of_string @@ Int64.to_string @@ Int64.succ c >>= fun () ->
+              Lwt.return c
+            | None ->
+              Store.insert store key @@ Store.value_of_string "1" >>= fun () ->
+              Lwt.return 1L
+
+  (* given a URI, find the appropriate file,
+   * and construct a response with its contents. *)
+  let rec dispatcher store uri =
+    match Uri.path uri with
+    | "" | "/" -> dispatcher store (Uri.with_path uri "index.html")
+    | "/index.html" ->
+      let headers = Cohttp.Header.init_with "Content-Type" "text/html"
+      in
+      next_camel store >>= fun counter ->
+      let body = Fmt.strf {html|<html>
+<body>
+<pre>
+                     ,,__
+           ..  ..   / o._)                   .---.
+          /--'/--\  \-'||        .----.    .'     '.
+         /        \_/ / |      .'      '..'         '-.
+       .'\  \__\  __.'.'     .'          ì-._
+         )\ |  )\ |      _.'
+        // \\ // \\
+       ||_  \\|_  \\_
+   mrf '--' '--'' '--'
+
+       %Ld camels served!
+</pre>
+</body></html>
+|html} (counter)
+      in
+      S.respond_string ~status:`OK ~body ~headers ()
+    | _ ->
+      S.respond_not_found ()
+
+  let serve dispatch =
+    let callback (_, cid) request _body =
+      let uri = Cohttp.Request.uri request in
+      let cid = Cohttp.Connection.to_string cid in
+      Http_log.info (fun f -> f "[%s] serving %s." cid (Uri.to_string uri));
+      dispatch uri
+    in
+    let conn_closed (_,cid) =
+      let cid = Cohttp.Connection.to_string cid in
+      Http_log.info (fun f -> f "[%s] closing" cid);
+    in
+    S.make ~conn_closed ~callback ()
+
+end
+
+module HTTP
+    (Pclock: Mirage_types.PCLOCK) (Http: HTTP) (B: BLOCK) =
+struct
+
+  module B2 = struct
+    include B
+    let discard _ _ _ = Lwt.return (Rresult.R.return ())
+  end
+
+  module Store = Wodan.Make(B2)(Wodan.StandardSuperblockParams)
+  module D = Dispatch(Store)(Http)
+
+  let start _clock http block =
+    let http_port = Key_gen.http_port () in
+    let tcp = `TCP http_port in
+    let http =
+      Http_log.info (fun f -> f "listening on %d/TCP" http_port);
+(*      let () = Logs.set_level (Some Logs.Debug); *)
+      Store.prepare_io Wodan.OpenExistingDevice block Wodan.standard_mount_options
+      >>= fun (store, _) ->
+      Http_log.info (fun f -> f "store done");
+      http tcp @@ D.serve (D.dispatcher store)
+    in
+    Lwt.join [ http ]
+
+end
