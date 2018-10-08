8f8c7d15e17ad712554ac0f3fcfcfdd8792abd95
Gabriel <g2p.code@gmail.com>
2018-10-08 17:40:14 +0100

    Import the local Git repo, then flush

--- src/wodan-irmin/bin/wodan_git_import.ml
+++ src/wodan-irmin/bin/wodan_git_import.ml
@@ -41,8 +41,18 @@ let run () =
   let%lwt () = Nocrypto_entropy_lwt.initialize () in
   let%lwt wodan_repo = Wodan_S.Repo.v wodan_config in
   let%lwt git_repo = Git_S.Repo.v git_config in
+  let%lwt git_master = Git_S.master git_repo in
+  let remote = Irmin.remote_store (module Git_S) git_master in
+  let%lwt wodan_master = Wodan_S.master wodan_repo in
+  let%lwt head_commit =
+    Wodan_sync.fetch_exn wodan_master remote in
+  let%lwt () = Wodan_S.Head.set wodan_master head_commit in
+  let%lwt wodan_raw = Wodan_S.DB.v wodan_config in
+  let%lwt _gen = Wodan_S.DB.flush wodan_raw in
   Lwt.return_unit
 
 let () =
+  Logs.set_reporter @@ Logs.format_reporter ();
+  Logs.set_level @@ Some Logs.Debug;
   Lwt_main.run @@ run ()
 
