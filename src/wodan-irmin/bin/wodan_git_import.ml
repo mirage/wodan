9729d99b5aa17268c5c46b33583f87e71d8c4f4b
Gabriel <g2p.code@gmail.com>
2018-10-09 12:10:57 +0100

    Logging fixes a fun race condition

--- src/wodan-irmin/bin/wodan_git_import.ml
+++ src/wodan-irmin/bin/wodan_git_import.ml
@@ -48,10 +48,15 @@ let run () =
   let%lwt () = Nocrypto_entropy_lwt.initialize () in
   let%lwt wodan_repo = Wodan_S.Repo.v wodan_config in
   StrHash.add persist_handles "keep it!" wodan_config;
+  Logs.info (fun m -> m "Ici");
   let%lwt git_repo = Git_S.Repo.v git_config in
+  Logs.info (fun m -> m "Là");
   let%lwt git_master = Git_S.master git_repo in
+  Logs.info (fun m -> m "Coucou");
   let remote = Irmin.remote_store (module Git_S) git_master in
+  Logs.info (fun m -> m "Toujours en vie");
   let%lwt wodan_master = Wodan_S.master wodan_repo in
+  Logs.info (fun m -> m "Plop");
   let%lwt _git_head = Git_S.Head.get git_master in
   let%lwt git_heads = Git_S.Head.list git_repo in
   begin
@@ -62,20 +67,20 @@ let run () =
          fun m -> m "Found a head")
       git_heads;
     begin if git_heads = [] then
-      Logs.debug @@ fun m -> m "Found no head";
+      Logs.info @@ fun m -> m "Found no head";
     end;
     let%lwt head_commit =
       Wodan_sync.fetch_exn wodan_master remote in
     let%lwt () = Wodan_S.Head.set wodan_master head_commit in
     let%lwt wodan_raw = Wodan_S.DB.v wodan_config in
     let%lwt _gen = Wodan_S.DB.flush wodan_raw in
-    Logs.debug (fun m -> m "persist_handles %d" @@ StrHash.length persist_handles);
+    Logs.info (fun m -> m "persist_handles %d" @@ StrHash.length persist_handles);
     Lwt.return_unit
   end
 
 let () =
   Logs.set_reporter @@ Logs.format_reporter ();
   Logs.set_level @@ Some Logs.Info;
-  Logs.debug @@ fun m -> m "Pwd %s" @@ Sys.getcwd ();
+  Logs.info @@ fun m -> m "Pwd %s" @@ Sys.getcwd ();
   Lwt_main.run @@ run ()
 
