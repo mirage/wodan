9ca274b57d3866232798e8f6f8348b1caff4de96
Gabriel <g2p.code@gmail.com>
2018-10-09 10:11:48 +0100

    Try harder to persist config handles

--- src/wodan-irmin/bin/wodan_git_import.ml
+++ src/wodan-irmin/bin/wodan_git_import.ml
@@ -44,9 +44,13 @@ let git_config = Irmin_git.config Sys.argv.(1)
 
 module Wodan_sync = Irmin.Sync(Wodan_S)
 
+module StrHash = Hashtbl.Make(struct include String let hash=Hashtbl.hash end)
+let persist_handles = StrHash.create 1
+
 let run () =
   let%lwt () = Nocrypto_entropy_lwt.initialize () in
   let%lwt wodan_repo = Wodan_S.Repo.v wodan_config in
+  StrHash.add persist_handles "keep it!" wodan_config;
   let%lwt git_repo = Git_S.Repo.v git_config in
   let%lwt git_master = Git_S.master git_repo in
   let remote = Irmin.remote_store (module Git_S) git_master in
@@ -68,6 +72,7 @@ let run () =
     let%lwt () = Wodan_S.Head.set wodan_master head_commit in
     let%lwt wodan_raw = Wodan_S.DB.v wodan_config in
     let%lwt _gen = Wodan_S.DB.flush wodan_raw in
+    Logs.debug (fun m -> m "persist_handles %d" @@ StrHash.length persist_handles);
     Lwt.return_unit
   end
 
