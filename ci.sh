1a114fa1085d2b1e98b40b1d4a28016e5bb2cf9f
Gabriel de Perthuis <g2p.code@gmail.com>
2020-10-01 17:04:06 +0200

    Update CI to work with latest versions of OCaml
    Unfortunately, rootless podman/buildah breaks due to races
    in build parallelism when building dune itself.

--- ci.sh
+++ ci.sh
@@ -3,6 +3,7 @@
 # Useful for testing things without waiting on Travis,
 # but only slightly faster
 # Using rootless podman, should work similarly with docker
-# See CI-NOTES
-podman build --build-arg OCAML_VERSION=4.09 . -v ~/.opam/download-cache:/home/opam/.opam/download-cache -v $PWD:/repo --tag local-build
+# See doc/CI-NOTES.
+#podman build -v ~/.opam/download-cache:/home/opam/.opam/download-cache -v $PWD:/repo --tag local-build .
+buildah bud -v ~/.opam/download-cache:/home/opam/.opam/download-cache -v $PWD:/repo --tag local-build .
 podman run -it --userns=keep-id -v ~/.opam/download-cache:/home/opam/.opam/download-cache -v .:/repo -e PACKAGE=wodan-irmin -e PINS="$({ echo wodan.dev:.; for op in vendor/*/*.opam; do echo "$op" |sed -re 's#(vendor/.*/)(.*)\.opam#\2.dev:\1#'; done; } |xargs)" local-build ci-opam
